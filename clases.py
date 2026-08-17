e8d6667091a3b91ec91226f087f51d0acf1c3819
ferdeveloper05 <fer.developer05@gmail.com>
2026-08-16 23:02:19 -0400

    Script organizador de archivos y directorios

---
+++ clases.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+class ReglaOrganizador:
+     
+    def __init__(self, nombre_categoria: str, extensiones: list):
+        pass
+    
+    def coincide(self, path_dir: Path) -> bool: 
+        pass
