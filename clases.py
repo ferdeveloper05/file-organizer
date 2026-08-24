32ce0294c8f8c3e6dcac4cd0487ffe79cdc6bfd1
ferdeveloper05 <fer.developer05@gmail.com>
2026-08-23 22:52:49 -0400

    Desarrollando logica de las clases

--- clases.py
+++ clases.py
@@ -3,8 +3,19 @@ from pathlib import Path
 
 class ReglaOrganizador:
      
-    def __init__(self, nombre_categoria: str, extensiones: list):
-        pass
+    def __init__(self, nombre_categoria: str, extensiones: list[str]):
+        self.nombre_categoria = nombre_categoria
+        self._extensiones = extensiones
+    
     
     def coincide(self, path_dir: Path) -> bool: 
         pass
+    
+    def __str__(self):
+        return f"{self.nombre_categoria} = {self.extensiones}"
+    
+exten = [".pdf", "docx", ".txt"]
+
+regla = ReglaOrganizador("Documento", exten)
+
+print(regla)
